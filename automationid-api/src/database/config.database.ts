05648a9ec02f3e84bf9a01eacfde798e72c15abc
Dramane <Dramane.dev@gmail.com>
2022-06-19 19:42:30 +0200

    Docker compose updated

--- automationid-api/src/database/config.database.ts
+++ automationid-api/src/database/config.database.ts
@@ -1,12 +1,21 @@
 import { Sequelize } from "sequelize";
 import config from "./default";
 
-export const database = new Sequelize(config.mySqlDatabaseName, config.mySqlUser, config.mySqlPassword, {
-    host: config.mySqlHost,
-    port: config.mysqlPort,
+export const database = new Sequelize("idea_collector", "userAutomationid", "rootroot", {
+    host: "automationid-db",
+    port: 3307,
     dialect: "mysql",
     logging: false,
     define: {
         timestamps: false,
     },
 });
+// export const database = new Sequelize(config.mySqlDatabaseName, config.mySqlUser, config.mySqlPassword, {
+//     host: config.mySqlHost,
+//     port: config.mysqlPort,
+//     dialect: "mysql",
+//     logging: false,
+//     define: {
+//         timestamps: false,
+//     },
+// });
