91abe07bc2b2718293b5d9025784dfaa30875e32
Dramane <Dramane.dev@gmail.com>
2022-06-19 19:26:14 +0200

    Docker compose updated

--- automationid-api/src/database/config.database.ts
+++ automationid-api/src/database/config.database.ts
@@ -3,7 +3,6 @@ import config from "./default";
 
 export const database = new Sequelize(config.mySqlDatabaseName, config.mySqlUser, config.mySqlPassword, {
     host: config.mySqlHost,
-    port: config.mysqlPort,
     dialect: "mysql",
     logging: false,
     define: {
--- automationid-api/src/database/database.connection.ts
+++ automationid-api/src/database/database.connection.ts
@@ -4,6 +4,6 @@ export const connectionToDatabase = (): Promise<string> => {
     return new Promise((resolve, reject) => {
         database.authenticate()
         .then(() => resolve("Database connection established ✅"))
-        .catch((error) => reject(`Database connection error ❌ : ${error} ${error.message}`));
+        .catch((error) => reject(`Database connection error ❌ : ${error.message}`));
     });
 }
